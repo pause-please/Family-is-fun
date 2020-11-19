ea0fde77d79a1162f23e572362b9b29de33d16ff
Pause Please <74709395+pause-please@users.noreply.github.com>
2020-11-19 20:40:14 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,35 @@
+/* MARQUEE */
+var rightJS = {
+  init: function(){
+    rightJS.Tags = document.querySelectorAll('.rightJS');
+    for(var i = 0; i < rightJS.Tags.length; i++){
+      rightJS.Tags[i].style.overflow = 'hidden';
+    }
+    rightJS.Tags = document.querySelectorAll('.rightJS div');
+    for(var i = 0; i < rightJS.Tags.length; i++){
+      rightJS.Tags[i].style.position = 'relative';
+      rightJS.Tags[i].style.right = '-'+rightJS.Tags[i].parentElement.offsetWidth+'px';
+    }
+    rightJS.loop();
+  },
+  loop: function(){
+    for(var i = 0; i < rightJS.Tags.length; i++){
+      var x = parseFloat(rightJS.Tags[i].style.right);
+      x ++;
+      var W = rightJS.Tags[i].parentElement.offsetWidth;
+      var w = rightJS.Tags[i].offsetWidth;
+      if((x/100) * W  > w) x = -W;
+      if (rightJS.Tags[i].parentElement.parentElement.querySelector(':hover') !== rightJS.Tags[i].parentElement) rightJS.Tags[i].style.right = x + 'px';
+    } 
+    requestAnimationFrame(this.loop.bind(this));
+  }
+};
+window.addEventListener('load',rightJS.init);
+
+
+
+/* PP TEXT */
+
 var x = 0, y = 0;
 var stepSize = 0.0;
 var letters = "Pause Please. →→→→→ Things you can see only when you pause. All inspiration comes from home. Ordinary life living it day by day like anyone else. ☆★   ";
